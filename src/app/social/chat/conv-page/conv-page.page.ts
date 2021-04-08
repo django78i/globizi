f2027f402350b84aab9835391a9d8af585b0c4ab
serigne20 <60139798+serigne20@users.noreply.github.com>
2021-04-08 10:40:01 +0200

    chatV1

--- src/app/social/chat/conv-page/conv-page.page.ts
+++ src/app/social/chat/conv-page/conv-page.page.ts
@@ -1,5 +1,11 @@
 import { NavController } from '@ionic/angular';
+import { Observable } from 'rxjs/internal/Observable';
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { BehaviorSubject } from 'rxjs';
+import firebase from 'firebase/app';
+import { analyzeAndValidateNgModules } from '@angular/compiler';
 
 @Component({
   selector: 'app-conv-page',
@@ -8,13 +14,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConvPagePage implements OnInit {
 
-  constructor(private navCtl: NavController) { }
+  messageText: any;
+  userID : any;
+  messages = [
+		{
+			date: "2021-03-28T15:18:22.521Z",
+			text: "Salut j’ai vu que tu as déjà visit..",
+      userId: "imvmCuh4bLhMp1Za5juJ12LjJpd2"
+		},
+		{
+      date: "2021-03-28T15:20:22.521Z",
+			text: "bonjour oui en effet j'ai visité ..........",
+      userId: "0123456789"
+		},
+		{
+			date: "2021-03-28T15:22:22.521Z",
+			text: "j'aimerai bien faire ce voyage ...........",
+      userId: "imvmCuh4bLhMp1Za5juJ12LjJpd2"
+		},
+		{
+			date: "2021-03-28T15:25:22.521Z",
+			text: "je te le conseil ..........",
+      userId: "0123456789"
+		}
+	]
+  
+  messagesSub: BehaviorSubject<any[]> = new BehaviorSubject(null);
+  messagesObs$: Observable<any[]>;
+
+  constructor(
+    private navCtl: NavController,
+    private afs: AngularFirestore,
+    public afAuth: AngularFireAuth,
+    
+  ){
+
+    this.messagesSub.next(this.messages);
+    this.messagesObs$ = this.messagesSub;
+
+    this.afAuth.authState.subscribe(auth => {
+      if(!auth){
+        console.log("nop")
+      }else{
+        console.log('uti :'+auth.uid);
+        this.userID = auth.uid;
+      }
+    })
+    
+
+    }
+    
+  
 
   ngOnInit() {
   }
 
   nav() {
     this.navCtl.navigateBack(['']);
+  }
+
+  sendMessage(){
+    console.log('messageText: '+ this.messageText );
+    console.log(this.userID);
+    const date= new Date().toISOString();
+    console.log(date);
+    this.messageText = '';
   }
 
 }
